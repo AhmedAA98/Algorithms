b1d7b08eedd742ec2c8d5d2552d370a79e0240ab
AAAmsb <aaamsb98@gmail.com>
2022-10-05 23:52:03 +0200

    test commit

---
+++ bubbleSort.cpp
@@ -0,0 +1,74 @@
+
+#include <iostream>
+using namespace std;
+
+void bubbleSortOptimize(int array[], int size) {
+    
+  for (int step = 0; step < (size-1); ++step) {
+      
+    int swapped = 0;
+    
+    for (int i = 0; i < (size-step-1); ++i) {
+        
+      if (array[i] > array[i + 1]) {
+
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        
+        swapped = 1;
+      }
+    }
+    if (swapped == 0)
+      break;
+  }
+}
+
+// print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    cout << "  " << array[i];
+  }
+  cout << "\n";
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9};
+  
+  // find the array's length
+  int size = sizeof(data) / sizeof(data[0]);
+  
+  bubbleSort(data, size);
+  
+  cout << "Sorted Array in Ascending Order:\n";
+  printArray(data, size);
+}
+void bubbleSort(int array[], int size) {
+
+  for (int step = 0; step < size; ++step) {
+
+    for (int i = 0; i < size - step; ++i) {
+      if (array[i] > array[i + 1]) {
+
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  }
+}
+
+// print array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    cout << "  " << array[i];
+  }
+  cout << "\n";
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9};
+  int size = sizeof(data) / sizeof(data[0]);
+  bubbleSort(data, size);
+  printArray(data, size);
+}
